3805940941339401b4e9d847159333151c26ca12
R44VC0RP <n01508660@unf.edu>
2024-09-28 17:33:18 -0400

    update nginx

--- start.sh
+++ start.sh
@@ -7,4 +7,12 @@ python3 webapp.py &
 
 echo "Starting nginx..."
 # Start nginx in the foreground
-nginx -g 'daemon off;'
+nginx -g 'daemon off;' &
+
+# Wait for the webapp to start
+sleep 10
+
+echo "Webapp and nginx started"
+
+# Keep the script running
+tail -f /dev/null
